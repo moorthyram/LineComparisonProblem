244f9360c6737780c2684ddd69a3b687e9080b07
Ramamoorthy <ramamoorthy300798@gmail.com>
2022-07-21 11:10:58 +0530

    Empwage

---
+++ EmpWageComparison.cs
@@ -0,0 +1,38 @@
+﻿namespace LineComparisionProblem
+
+{
+    class EmpWageComputation
+    {
+        static void Empwage(string[] args)
+        {
+            int employeePresent = 2;
+            int employeePartTimePresent = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empWage = 0;
+            int empHrs = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if (employeePartTimePresent == empCheck)
+            {
+                Console.WriteLine("Employee is present");
+                empHrs = 4;
+            }
+            else if (employeePresent == empCheck)
+            {
+                Console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
